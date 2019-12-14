988a3562f46b5c662f1b7c39b77448d52dc35da5
emira <emirahajj@gmail.com>
2019-12-13 23:05:05 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <regex>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <iostream>
+#include "CodewordGenerator.hpp"
+
+
+int main()
+{
+	std::string a = "[A-Z]+BB[Y-Z]+"; //the pattern we want to search for
+	//first one should be ABBY
+	std::string b = "[C-M]+[A-Y]+[G-O]*B";
+	//should be NMJ
+	CodewordGenerator mygen;
+	std::string answer;
+	std::string answer2;
+	answer = mygen.generateShortestWord(a);
+	std::cout<<answer<<std::endl;
+	answer2= mygen.generateLengthWord(b, 3);
+	std::cout<<answer2<<std::endl;
+
+return 0;
+
+}
